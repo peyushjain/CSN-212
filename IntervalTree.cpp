11d39ebeef5b2374174c6837920f9a2ebe51ac31
peyushjain <peyushjain4@gmail.com>
2017-02-17 00:57:19 +0530

    Update IntervalTree.cpp

--- IntervalTree.cpp
+++ IntervalTree.cpp
@@ -9,22 +9,25 @@ struct Interval{
 	int low;
 	int high;
 };
-
+// creating node of interval tree
 struct node{
 	Interval interval;
 	node* left;
 	node* right;
-	int maxI; // stores max of hig
+	int maxI; // stores max of high[root],max(left subtree, right subtree)
 };
 
-node* create(Interval input){
+// creating new node
+node* create(Interval input){    
 	node* n=new node;
 	n->interval=input;
 	n->left=NULL;
 	n->right=NULL;
 	n->maxI=input.high;
 }
-node* insert(node* root,Interval input){   // assuming interval tree is keyed at low
+
+// insert operation in interval tree. assuming interval tree is keyed at low
+node* insert(node* root,Interval input){   
    // base case
    if(root==NULL) return create(input);
    
@@ -44,6 +47,8 @@ node* insert(node* root,Interval input){   // assuming interval tree is keyed at
    
    return root;
 }
+
+// preorder traversal of interval tree.
 void preorder(node *root)
 {
 	if (root == NULL) return;
@@ -52,6 +57,7 @@ void preorder(node *root)
     preorder(root->right);
 }
 
+// searching for an overlapping interval. return true if overlapping interval  is found  (also prints overlapping interval) else return false. 
 bool search( node* root, Interval input){
 	
 	while(root!=NULL && ( (input.low > root->interval.high) || (input.high < root->interval.low))){ 	
@@ -73,6 +79,8 @@ bool search( node* root, Interval input){
 bool equal(Interval i,Interval j){	
    return (i.low==j.low && i.high==j.high);	
 }
+
+// function to return successor node of a node in interval tree.
 node* successor(node* root){
 	node* current = root;
     // loop down to find the leftmost leaf 
@@ -81,6 +89,8 @@ node* successor(node* root){
     }
     return current;
 }
+
+// function to delete node from interval tree. assuming interval to be deleted is given as input.
 node* deleteNode( node* root, Interval input){
 	
 	if(root==NULL) return root;
@@ -139,6 +149,7 @@ int main(){
 		cout<<"No overlapping intervals"<<endl;
 	}
     
+    // performing node deletion operation
     Interval del = {5,20};
     root=deleteNode(root,del);
     preorder(root);	
